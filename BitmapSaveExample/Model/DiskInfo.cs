f72d4a002d7f7d1a21cc7992354e78162ae82c54
규민 최 <gyumin3931@naver.com>
2023-02-15 23:13:18 +0900

    Ver.1.0.0 완성

---
+++ BitmapSaveExample/Model/DiskInfo.cs
@@ -0,0 +1,41 @@
+﻿using BitmapSaveExample.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitmapSaveExample.Model
+{
+    public class DiskInfo : NotifyPropertyChanged
+    {
+        private string _modelName;
+        private string _discription;
+        private string _diskType;
+        private string _rootDirectory;
+        public string ModelName
+        {
+            get => _modelName;
+            set { _modelName = value; OnPropertyChanged(nameof(ModelName)); }
+        }
+
+        public string Discription
+        {
+            get => _discription;
+            set { _discription = value; OnPropertyChanged(nameof(Discription)); }
+        }
+        public string DiskType
+        {
+            get => _diskType;
+            set { _diskType = value; OnPropertyChanged(nameof(DiskType)); }
+        }
+        public string RootDirectory
+        {
+            get => _rootDirectory;
+            set { _rootDirectory = value; OnPropertyChanged(nameof(RootDirectory)); }
+        }
+        public DiskInfo()
+        {
+        }
+    }
+}
--- BitmapSaveExample/ViewModels/MainViewModel.cs
+++ BitmapSaveExample/ViewModels/MainViewModel.cs
@@ -1,18 +1,28 @@
-﻿using BitmapSaveExample.Service;
+﻿using BitmapSaveExample.Model;
+using BitmapSaveExample.Service;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Management;
+using System.Windows;
 using System.Windows.Input;
 
 namespace BitmapSaveExample.ViewModels
 {
     public class MainViewModel : NotifyPropertyChanged
     {
+        const string TestSavePath = "TestImagePath";
+
         private string _imagePath;
-        private string _saveTime;
+        private TimeSpan _savingTime;
         private string _diskType;
+        private string _modelName;
+        private string _discription;
+        private int _saveCount;
 
         public string ImagePath
         {
@@ -20,10 +30,10 @@ namespace BitmapSaveExample.ViewModels
             set { _imagePath = value; OnPropertyChanged(nameof(ImagePath)); }
         }
 
-        public string SaveTime
+        public TimeSpan SavingTime
         {
-            get => _saveTime;
-            set { _saveTime = value; OnPropertyChanged(nameof(SaveTime)); }
+            get => _savingTime;
+            set { _savingTime = value; OnPropertyChanged(nameof(SavingTime)); }
         }
 
         public string DiskType
@@ -32,26 +42,67 @@ namespace BitmapSaveExample.ViewModels
             set { _diskType = value; OnPropertyChanged(nameof(DiskType)); }
         }
 
-        public List<string> DiskList { get; set; }
+        public string ModelName
+        {
+            get => _modelName;
+            set { _modelName = value; OnPropertyChanged(nameof(ModelName)); }
+        }
+
+        public string Discription
+        {
+            get => _discription;
+            set { _discription = value; OnPropertyChanged(nameof(Discription)); }
+        }
+
+        public List<DiskInfo> DiskList { get; set; }
 
         public ICommand BtnSetImagePathClick { get; set; }
-
+        public ICommand BtnSaveBitmapClick { get; set; }
         public MainViewModel()
         {
             BtnSetImagePathClick = new Command(OnUploadImagePath, CanExecute_Func);
-            DiskList = new List<string>();
-            Init();
+            BtnSaveBitmapClick = new Command(OnSaveBitmap, CanExecute_Func);
+
+            DiskList = new List<DiskInfo>();
+            _saveCount = 0;
+            Initialize();
         }
 
         private void OnUploadImagePath(object obj)
         {
-            ImagePath = GetFilePath("bmp");
-            if (ImagePath.Contains("C:")) DiskType = DiskList[1];
-            if (ImagePath.Contains("D:")) DiskType = DiskList[0];
+            string backup = ImagePath;
+            ImagePath = GetFilePath(backup, "bmp");
 
+            DiskInfo info = FindDiskInfo(ImagePath);
+            if (info == null) return;
+
+            DiskType = info.DiskType;
+            ModelName = info.ModelName;
+            Discription = info.Discription;
+
+            CreateSaveImagePath(info.RootDirectory);
         }
 
-        public string GetFilePath(string filter)
+        private void OnSaveBitmap(object obj)
+        {
+            DiskInfo info =  FindDiskInfo(ImagePath);
+            if(info == null) return;
+
+            string extention = Path.GetExtension(ImagePath);
+            string testPath = SetPath(info.RootDirectory);
+            string savePath = Path.Combine(testPath, _saveCount.ToString());
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            using (Bitmap bmp = new Bitmap(ImagePath))
+            {
+                bmp.Save(savePath + extention);
+                stopwatch.Stop();
+                SavingTime = stopwatch.Elapsed;
+            }
+            _saveCount++;
+        }
+
+        public string GetFilePath(string backup, string filter)
         {
             var fileDialog = new OpenFileDialog()
             {
@@ -60,42 +111,110 @@ namespace BitmapSaveExample.ViewModels
             };
 
             if (fileDialog.ShowDialog() == true) return fileDialog.FileName;
-            else return string.Empty;
+            else return backup;
+        }
+
+        private string SetPath(string localPath)
+        {
+            string path = Path.Combine(localPath, TestSavePath);
+            return path;
+        }
+
+        private DiskInfo FindDiskInfo(string path)
+        {
+            try
+            {
+                char rootName = path.FirstOrDefault();
+                var info = DiskList.Single(item => item.RootDirectory.First() == rootName);
+
+                return info;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+                return null;
+            }
+            
+        }
+
+        //TODO : Disk 여러개 있는 곳에서 Test 해봐야함
+        private void Initialize()
+        {
+            try
+            {
+                DriveInfo[] driveInfo = DriveInfo.GetDrives();
+                int length = driveInfo.Length;
+                for (int i = 0; i < length; i++)
+                {
+                    var diskInfo = new DiskInfo() { RootDirectory = driveInfo[i].RootDirectory.ToString() };
+
+                    DiskList.Add(diskInfo);
+                }
+
+                ManagementScope scope = new ManagementScope(@"\\.\root\microsoft\windows\storage");
+                scope.Connect();
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from MSFT_PhysicalDisk"))
+                {
+                    searcher.Scope = scope;
+                    int index = length;
+                    foreach (ManagementObject item in searcher.Get())
+                    {
+                        string diskType = "";
+                        index--;
+
+                        switch (Convert.ToInt16(item["MediaType"]))
+                        {
+                            case 1:
+                                diskType = "Unspecified";
+                                break;
+
+                            case 3:
+                                diskType = "HDD";
+                                break;
+
+                            case 4:
+                                diskType = "SSD";
+                                break;
+
+                            case 5:
+                                diskType = "SCM";
+                                break;
+
+                            default:
+                                diskType = "Unspecified";
+                                break;
+                        }
+                        DiskList[index].DiskType = diskType;
+                    }
+                }
+
+                WqlObjectQuery q = new WqlObjectQuery("SELECT * FROM Win32_DiskDrive");
+                using (ManagementObjectSearcher res = new ManagementObjectSearcher(q))
+                {
+                    int index = length;
+                    foreach (ManagementObject o in res.Get())
+                    {
+                        index--;
+
+                        DiskList[index].ModelName = (string)o["Model"];
+                        DiskList[index].Discription = (string)o["Description"];
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());  
+            }
+            
+        }
+
+        private void CreateSaveImagePath(string localPath)
+        {
+            string testPath = SetPath(localPath);
+
+            if (!Directory.Exists(testPath)) Directory.CreateDirectory(testPath);
         }
 
         private bool CanExecute_Func(object arg) => true;
-        private void Init()
-        {
-            ManagementScope scope = new ManagementScope(@"\\.\root\microsoft\windows\storage");
-            scope.Connect();
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from MSFT_PhysicalDisk");
-            searcher.Scope = scope;
-            foreach (ManagementObject item in searcher.Get())
-            {
-                switch (Convert.ToInt16(item["MediaType"]))
-                {
-                    case 1:
-                        DiskList.Add("Unspecified");
-                        break;
-
-                    case 3:
-                        DiskList.Add("HDD");
-                        break;
-
-                    case 4:
-                        DiskList.Add("SSD");
-                        break;
-
-                    case 5:
-                        DiskList.Add("SCM");
-                        break;
-
-                    default:
-                        DiskList.Add("Unspecified");
-                        break;
-                }
-            }
-            searcher.Dispose();
-        }
     }
 }
